b524c083568afcdbbb8ba6320f57bfb7b4ff9fa6
Rahul Thakur <rahulthakur77752@gmail.com>
2026-04-18 17:18:04 +0530

    Added Basic controller Structure

---
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -0,0 +1,23 @@
+package com.example.TodoApiSpring;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+// Two @ controller + @RequestBody == Restcontroller
+// Controller -> Entry point that collects request
+// usage : collecting the request and returning the response
+// does not have any business logic here.
+// written the response in http response body.
+public class TodoController {
+    private static List<Todo> todos;
+    public TodoController(){
+        todos = new ArrayList<>();
+        todos.add(new Todo(1,false , "Todo -1",1));
+        todos.add(new Todo(2,true,"Todo-2",2));
+
+    }
+
+}
